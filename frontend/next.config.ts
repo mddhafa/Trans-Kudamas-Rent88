c00f94dd8bcb58a949d60fba54a26fb75b1a093e
alviennn <alvien.rnp@gmail.com>
2026-06-04 22:38:25 +0700

    simplify next config for vercel deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Allow HMR / dev resources to be loaded from these origin(s).
   // Update to the IP/port you use to access the dev server from another device.
-  allowedDevOrigins: [
-    "http://192.168.18.100:3000",
-    "http://192.168.18.100:3001",
-  ],
 };
 
 export default nextConfig;
