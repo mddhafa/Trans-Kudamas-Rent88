4cce3c5da2a9ef002e8bc559b911b9c9e7438672
alviennn <alvien.rnp@gmail.com>
2026-06-04 22:19:02 +0700

    check

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,6 +15,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export const metadata: Metadata = {
   title: "KudaMas Rent 88",
   description: "Rental mobil profesional untuk kebutuhan bisnis dan keluarga",
